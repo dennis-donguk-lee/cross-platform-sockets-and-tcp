2f83e9a8e6996ac1394b13bbffd4dde665f7612a
Dennis Lee <dennis.donguk.lee@gmail.com>
2018-10-30 16:29:55 -0700

    Create a Windows socket.

--- Main.cpp
+++ Main.cpp
@@ -10,6 +10,23 @@ int init_winsock()
   return WSAStartup(MAKEWORD(2, 2), &wsa);
 }
 
+SOCKET create_socket(const int protocol)
+{
+    // UDP datagram socket type
+    auto type = SOCK_DGRAM;
+    // Handle TCP protocol.
+    if (protocol == IPPROTO_TCP)
+    {
+        type = SOCK_STREAM;
+    }
+
+    // Return INVALID_SOCKET when failed in which case call WSAGetLastError().
+    auto res = INVALID_SOCKET;
+    res = socket(AF_INET, type, protocol);
+
+    return res;
+}
+
 int main()
 {
   if (init_winsock() != 0)
@@ -17,6 +34,10 @@ int main()
     std::cout << "WSAStartup failed. Error code: " << WSAGetLastError();
     exit(EXIT_FAILURE);
   }
+
+  // Create a socket and an address.
+  const auto sock = create_socket(IPPROTO_TCP);
+  //const auto addr = CreateAddress("104.131.138.5", PORT);
 
   return 0;
 }
