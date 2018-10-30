b42406428a9a590fc1e788d07b0764b968bcc835
Dennis Lee <dennis.donguk.lee@gmail.com>
2018-10-30 16:48:11 -0700

    Create an address.

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,4 @@
 #pragma comment(lib, "Ws2_32.lib")
-
 #include <WS2tcpip.h>
 #include <iostream>
 
@@ -10,6 +9,7 @@ int init_winsock()
   return WSAStartup(MAKEWORD(2, 2), &wsa);
 }
 
+// Create a socket.
 SOCKET create_socket(const int protocol)
 {
     // UDP datagram socket type
@@ -21,10 +21,33 @@ SOCKET create_socket(const int protocol)
     }
 
     // Return INVALID_SOCKET when failed in which case call WSAGetLastError().
-    auto res = INVALID_SOCKET;
-    res = socket(AF_INET, type, protocol);
+    return socket(AF_INET, type, protocol);
+}
 
-    return res;
+// Create an address.
+sockaddr_in* create_address(const char* ip, const int port)
+{
+  // Allocate memory for the sockaddr in structure.
+  const auto addr = new sockaddr_in;
+  addr->sin_family = AF_INET;
+  // Remember to go from host byte order to network byte order.
+  addr->sin_port = htons(port);
+ 
+  if (ip == nullptr)
+  {
+    // Bind to all IP addresses local machine may have.
+    addr->sin_addr.S_un.S_addr = INADDR_ANY;
+  }
+  else
+  {
+    // Take dotted-decimal string and turn it into a 32-bit IP address,
+    // in network byte order. Return INADDR_NONE on fail.
+    //addr->sin_addr.S_un.S_addr = inet_addr(ip);
+    inet_pton(addr->sin_family, ip, &(addr->sin_addr));
+  }
+
+  return addr;
+  // Caller will be responsible for free().
 }
 
 int main()
@@ -37,7 +60,7 @@ int main()
 
   // Create a socket and an address.
   const auto sock = create_socket(IPPROTO_TCP);
-  //const auto addr = CreateAddress("104.131.138.5", PORT);
+  const auto addr = create_address("104.131.138.5", 8888);
 
   return 0;
 }
