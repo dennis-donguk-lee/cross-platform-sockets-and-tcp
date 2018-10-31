7fd57a35bc16f935d64b074872fc460bdaf52200
Dennis Lee <dennis.donguk.lee@gmail.com>
2018-10-30 18:57:52 -0700

    Connect the socket and the address and send the data.

--- Main.cpp
+++ Main.cpp
@@ -1,39 +1,40 @@
 #pragma comment(lib, "Ws2_32.lib")
 #include <WS2tcpip.h>
 #include <iostream>
+#include <cassert>
 
 // Initialize Winsock.
 int init_winsock()
 {
-	WSADATA wsa;
+  WSADATA wsa;
   return WSAStartup(MAKEWORD(2, 2), &wsa);
 }
 
 // Create a socket.
 SOCKET create_socket(const int protocol)
 {
-    // UDP datagram socket type
-    auto type = SOCK_DGRAM;
-    // Handle TCP protocol.
-    if (protocol == IPPROTO_TCP)
-    {
-        type = SOCK_STREAM;
-    }
+  // UDP datagram socket type
+  auto type = SOCK_DGRAM;
+  // Handle TCP protocol.
+  if (protocol == IPPROTO_TCP)
+  {
+    type = SOCK_STREAM;
+  }
 
-    auto res = socket(AF_INET, type, protocol);
-    // Make it a non-blocking socket.
-    if (res != INVALID_SOCKET && protocol == IPPROTO_TCP)
+  auto res = socket(AF_INET, type, protocol);
+  // Make it a non-blocking socket.
+  if (res != INVALID_SOCKET && protocol == IPPROTO_TCP)
+  {
+    auto mode = 1ul;
+    if (ioctlsocket(res, FIONBIO, &mode) != NO_ERROR)
     {
-        auto mode = 1ul;
-        if (ioctlsocket(res, FIONBIO, &mode) != NO_ERROR)
-        {
-            closesocket(res);
-            res = INVALID_SOCKET;
-        }
+      closesocket(res);
+      res = INVALID_SOCKET;
     }
+  }
 
-    // INVALID_SOCKET when failed in which case call WSAGetLastError()
-    return res;
+  // INVALID_SOCKET when failed in which case call WSAGetLastError()
+  return res;
 }
 
 // Create an address.
@@ -44,7 +45,7 @@ sockaddr_in* create_address(const char* ip, const int port)
   addr->sin_family = AF_INET;
   // Remember to go from host byte order to network byte order.
   addr->sin_port = htons(port);
- 
+
   if (ip == nullptr)
   {
     // Bind to all IP addresses local machine may have.
@@ -62,7 +63,35 @@ sockaddr_in* create_address(const char* ip, const int port)
   // Caller will be responsible for free().
 }
 
-int main()
+int connect_socket_and_address(const SOCKET sock, sockaddr_in* addr)
+{
+  if (
+    connect(sock, reinterpret_cast<sockaddr*>(addr), sizeof(sockaddr_in))
+    == SOCKET_ERROR
+    )
+  {
+    return WSAGetLastError();
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+int send_tcp(const SOCKET sock, char const* buf, const int len)
+{
+  const auto res = send(sock, buf, len, 0);
+
+  if (res == SOCKET_ERROR)
+  {
+    return -1;
+  }
+
+  return res;
+}
+
+
+int main(int argc, char** argv)
 {
   if (init_winsock() != 0)
   {
@@ -73,6 +102,31 @@ int main()
   // Create a socket and an address.
   const auto sock = create_socket(IPPROTO_TCP);
   const auto addr = create_address("104.131.138.5", 8888);
+
+  // Connect the socket and the address.
+  auto err = connect_socket_and_address(sock, addr);
+  while (err == SOCKET_ERROR)
+  {
+    err = connect_socket_and_address(sock, addr);
+  }
+
+  // Get the sending buffer.
+  std::string sendbuf = argv[0];
+  const auto pos = sendbuf.find_last_of('\\');
+  sendbuf = sendbuf.substr(pos + 1);
+  const auto csendbuf = sendbuf.c_str();
+
+  // Send data over TCP and listen for a response.
+  auto sendlen = send_tcp(sock, csendbuf, sendbuf.length());
+  while (sendlen == SOCKET_ERROR)
+  {
+    sendlen = send_tcp(sock, csendbuf, sendbuf.length());
+  }
+  //char recvbuf[EMTU]{};
+  //const auto recvlen = Receive(sock, recvbuf, sizeof(recvbuf));
+  //assert(recvlen >= 0);
+
+  delete addr;
 
   return 0;
 }
